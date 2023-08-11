0bdcac8fd6f0c39ced7fa03ff2e898405a9aa3d3
Iamneo <techadmin@iamneo.ai>
2023-08-11 16:27:37 +0000

    Backup Commit

--- reactapp/src/components/UI/Card/Card.js
+++ reactapp/src/components/UI/Card/Card.js
@@ -4,15 +4,25 @@ const Card = (props) => {
   
   const getSelectedValue = (option) => {
     if (props.answer === option) {
-      return <></>
+      return option
     }
+    return option
   };
 
   return (
     <div>
       <div key={props.key}><h4>{props.question}</h4></div>
-        <div onClick={() => getSelectedValue(props.options.option1)}>
-              <Button>{props.options.option1}</Button>
+            <div onClick={() => getSelectedValue(props.options.option1)}>
+              <button>{props.options.option1}</button>
+            </div>
+            <div onClick={() => getSelectedValue(props.options.option2)}>
+              <button>{props.options.option2}</button>
+            </div>
+            <div onClick={() => getSelectedValue(props.options.option3)}>
+              <button>{props.options.option3}</button>
+            </div>
+            <div onClick={() => getSelectedValue(props.options.option4)}>
+              <button>{props.options.option4}</button>
             </div>
           
       </div>
